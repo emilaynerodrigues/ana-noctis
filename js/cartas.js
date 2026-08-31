e7b7dbb7dea958d01f42d8dc40a3e3d170531f12
Emilayne Rodrigues <63480241+emilaynerodrigues@users.noreply.github.com>
2026-08-31 08:41:22 -0400

    corrigindo

--- js/cartas.js
+++ js/cartas.js
@@ -10,7 +10,7 @@ Tudo continua sendo sobre -você-. Você ainda tem -tudo- de mim.
 
 Meu desejo ainda é: "Ela, ela e ela".
 
-Como é fácil pra você? Estou enlouquecendo de tanta saudades de você.
+Como é fácil para você? Estou enlouquecendo de tanta saudade de você.
 
 Será que tudo isso já pode acabar? Já não foi o suficiente?
 
@@ -23,9 +23,9 @@ Sinto sua falta.`,
     titulo: "Carta 2 - É a Aelin",
     texto: `Ana, Aelin realmente disse isso?
 
-    "Eu te amo. Além das estrelas, além do tempo, além da própria vida. Eu pertenço a você, e você pertence a mim. Não ha nada neste mundo, ou em qualquer outro, que possa mudar isso."
+    "Eu te amo. Além das estrelas, além do tempo, além da própria vida. Eu pertenço a você, e você pertence a mim. Não há nada neste mundo, ou em qualquer outro, que possa mudar isso."
 
-    Me indentifico um pouco com ela, porque tudo em mim é seu.
+    Me identifico um pouco com ela, porque tudo em mim é seu.
 
     Que mundo louco, não é?
 
@@ -53,9 +53,9 @@ Sinto sua falta.`,
   
       Que mundo louco, não é?
   
-     A verdade é que estou enlouquecendo de saudades.
+      A verdade é que estou enlouquecendo de saudades.
   
-    Seu cheiro, seu beijo, sua voz...`,
+      Seu cheiro, seu beijo, sua voz...`,
   },
 
   {
@@ -64,13 +64,13 @@ Sinto sua falta.`,
   
     "Eu te encontrei. No escuro, na tempestade, na dor mais profunda... Eu te encontrei e você me encontrou. Eu sou sua, Rowan. Sempre fui, desde o momento em que nossas almas se reconheceram."
   
-      Será loucura pensar que estavámos destinadas a se achar em meio ao caos da vida?
+      Será loucura pensar que estávamos destinadas a nos encontrar em meio ao caos da vida?
   
-     Acredito que não. Eu já sonhava com você e seus olhos.
+      Acredito que não. Eu já sonhava com você e seus olhos.
   
-     Tudo mudou, mas por que não ficar? 
+      Tudo mudou, mas por que não ficar?
   
-     Sinto sua falta.`,
+      Sinto sua falta.`,
   },
 
   {
@@ -79,7 +79,7 @@ Sinto sua falta.`,
   
     "Onde você for, meu coração vai junto. Não há distância ou guerra que possa diminuir o que sinto por você."
   
-     Você realmente anda por aí carregando algo meu.
+      Você realmente anda por aí carregando algo meu.
   
     Saudades.`,
   },
@@ -88,13 +88,13 @@ Sinto sua falta.`,
     titulo: "Carta 7 - Elide",
     texto: `Ana, é um tanto quanto:
   
-    "Eu não me importo com o seu passado ou com o que os outros dizem. Eu amo você, Lorcan, exatamente como voce é. E vou escolher você todas as vezes."
+    "Eu não me importo com o seu passado ou com o que os outros dizem. Eu amo você, Lorcan, exatamente como você é. E vou escolher você todas as vezes."
   
     Adorava ouvir você falar e rir das suas histórias passadas. E sinto ódio por cada um que te machucou.
   
-     As vezes fico pensando...
+      Às vezes fico pensando...
   
-     Eu continuo te escolhendo e não quero ser só uma passagem.
+      Eu continuo te escolhendo e não quero ser só uma passagem.
   
       Estou com saudades.`,
   },
@@ -103,7 +103,7 @@ Sinto sua falta.`,
     titulo: "Carta 8 - É a Celaena",
     texto: `Ana, ainda é:
   
-  "Eu sempre vou escolher você. Mesmo quando o mundo inteiro estiver rindo ou tentando nos separar meu coração é seu."
+  "Eu sempre vou escolher você. Mesmo quando o mundo inteiro estiver rindo ou tentando nos separar, meu coração é seu."
   
   Eu escolho você.
   
@@ -134,7 +134,7 @@ Sinto sua falta.`,
     titulo: "Carta 10 - É o Dorian",
     texto: `Ana,
 
-  "Você não é um monstro, Bruxa. E mesmo se fosse.. eu ainda assim não conseguiria afastar meus olhos de você. Você é a tempestade mais bonita que já vi."
+  "Você não é um monstro, Bruxa. E mesmo se fosse... eu ainda assim não conseguiria afastar meus olhos de você. Você é a tempestade mais bonita que já vi."
 
   Eu nunca enxerguei você pelas coisas que deram errado.
 
@@ -155,7 +155,7 @@ Sinto sua falta.`,
 
   Uma luz em uma época em que eu nem sabia direito que estava no escuro.
 
-  Talvez seja por isso seja tão difícil...
+  Talvez seja por isso que seja tão difícil...
 
   Eu não vou te esquecer e preciso de você perto.
 
@@ -170,7 +170,7 @@ Sinto sua falta.`,
     titulo: "Carta 12 - É o Lorcan",
     texto: `Ana,
 
-  "Eu vivi por séculos no escuro, servindo a obrigações e guerras que não eram minhas Mas por você, Elide... por você eu queimaria ○ mundo inteiro só para garantir que estivesse a salvo."
+  "Eu vivi por séculos no escuro, servindo a obrigações e guerras que não eram minhas. Mas por você, Elide... por você eu queimaria o mundo inteiro só para garantir que estivesse a salvo."
 
   Eu entendo um pouco.
 
@@ -185,7 +185,7 @@ Sinto sua falta.`,
 
      "I thought I'd never find you
       I convinced myself that I would never find you
-     When suddendly I saw you"
+      When suddenly I saw you"
 
   No meio de tanta gente, tantos caminhos e tantas coisas que poderiam ter acontecido, eu encontrei você.
 
@@ -204,31 +204,31 @@ Sinto sua falta.`,
     titulo: "Carta X - Estou com dor",
     texto: `Ana, sinto que escrevo cartas a um ser divino.
 
-Tudo em mim mudou desde que você disse que estava tão apaixonada por mim como eu por você. 
+Tudo em mim mudou desde que você disse que estava tão apaixonada por mim quanto eu por você. 
 
 Isso me fez lembrar de você sussurrando atrás de mim sobre alianças depois do boliche (eu ouvi).
 
 Eu realmente sou exagerada e já tinha escolhido qual seria. 
 
-Você vive nos meus pensamentos e me recuso a pensar que você não lembre de nada.
+Você vive nos meus pensamentos e me recuso a pensar que você não se lembre de nada.
 
 Continua sendo você. 
 
 Saudades,`,
   },
-  */
+*/
   {
     titulo: "Carta D - Despedida",
     texto: `Ana, 
 
 Somente eu e você sabemos o que foi dito, compartilhado e sentido.
 
-Não me resumo ao que foi dito e você sabe.
+Não me resumo ao que foi dito, e você sabe.
 
 Espero que fique bem e seja feliz em suas escolhas.
 
-Você sempre terá pra onde voltar.
+Você sempre terá para onde voltar.
 
-Existe uma lógica para a distribuição das cartas, espero que essa chegue a você.`,
+Existe uma lógica para a distribuição das cartas. Espero que essa chegue a você.`,
   },
 ];
